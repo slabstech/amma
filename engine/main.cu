ac4b9157676b3d0d92134f9eced80df98182347f
sachinsshetty <sachins911991@gmail.com>
2022-07-23 19:46:58 +0200

    Code compilation on ubuntu

--- engine/main.cu
+++ engine/main.cu
@@ -3,6 +3,8 @@
 #include "device_launch_parameters.h"
 
 #include <stdio.h>
+#include <iostream>
+using namespace std;
 
 cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size);
 
@@ -12,8 +14,72 @@ __global__ void addKernel(int *c, const int *a, const int *b)
     c[i] = a[i] + b[i];
 }
 
+
+__global__ void square(float *d_out, float *d_in){
+    int idx = threadIdx.x;
+    float f = d_in[idx];
+    d_out[idx] = f * f ;
+}
+__global__ void cube(float *d_out, float *d_in){
+    int idx = threadIdx.x;
+    float f = d_in[idx];
+    d_out[idx] = f * f * f ;
+}
+void task_square(){
+    const int ARRAY_SIZE = 64;
+    const int ARRAY_BYTES = ARRAY_SIZE * sizeof(float);
+
+    float h_in[ARRAY_SIZE];
+    for(int i=0;i < ARRAY_SIZE;i++){
+        h_in[i] = float(i);
+    }
+
+    float h_out[ARRAY_SIZE];
+
+    float * d_in;
+    float * d_out;
+
+
+    cout << "Running square" << endl;
+    cudaMalloc((void **) &d_in, ARRAY_BYTES);
+    cudaMalloc((void **) &d_out, ARRAY_BYTES);
+
+
+    cudaMemcpy(d_in, h_in, ARRAY_BYTES,cudaMemcpyHostToDevice );
+
+    square<<<1, ARRAY_SIZE>>>(d_out, d_in);
+
+    cudaMemcpy(h_out, d_out, ARRAY_BYTES,cudaMemcpyDeviceToHost );
+    for(int i=0;i < ARRAY_SIZE;i++){
+        cout << h_out[i] ;
+        cout << (((i%4)!=3) ? "\t" : "\n");
+    }
+
+// For cube
+    cout << "\n" ;
+    cout << "Running Cube" << endl;
+    for(int i=0;i < ARRAY_SIZE;i++){
+        h_in[i] = float(i);
+    }
+    cudaMemcpy(d_in, h_in, ARRAY_BYTES,cudaMemcpyHostToDevice );
+
+    cube<<<1, ARRAY_SIZE>>>(d_out, d_in);
+    cudaMemcpy(h_out, d_out, ARRAY_BYTES,cudaMemcpyDeviceToHost );
+    for(int i=0;i < ARRAY_SIZE;i++){
+        cout << h_out[i] ;
+        cout << (((i%4)!=3) ? "\t" : "\n");
+    }
+
+
+
+
+    cudaFree(d_in);
+    cudaFree(d_out);
+
+}
 int main()
 {
+    task_square();
     const int arraySize = 5;
     const int a[arraySize] = { 1, 2, 3, 4, 5 };
     const int b[arraySize] = { 10, 20, 30, 40, 50 };
